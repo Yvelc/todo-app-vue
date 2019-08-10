4a39f41e8ba3cbda16ab8e2e2584ef55d37e8e2f
rpadron1986 <rpadron1986@gmail.com>
2019-08-09 21:51:28 -0400

    Add todo list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
+<<<<<<< Updated upstream
         <h1 class="font-hairline text-6xl text-center text-blue-300 ">Todo App R&Y </h1>
 
         <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
@@ -8,6 +9,26 @@
 
         <todo-card class="mx-auto mt-10 w-full max-w-lg">
             Create/Edit/Eliminate Task
+=======
+        <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
+        
+            <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
+            <!--input for get a task -->
+            <input type="text" class="font-hairline w-full" placeholder="Enter your Task Here" v-model="newTodo" @keyup.enter="addTodo">
+            <div v-for="task in tasks" :key="task.id" class="flex content-start">
+                <div> 
+                    {{ task.title }}
+                </div>                
+                <div class="object-none object-right">
+                   &times;
+                </div>
+            </div>
+            
+        </todo-card>
+
+        <todo-card class="mx-auto mt-10 w-full max-w-lg">
+            Create/Edit Task 
+>>>>>>> Stashed changes
         </todo-card>
     </div>
 </template>
@@ -20,10 +41,17 @@ export default {
 
     data () {
         return {
+            newTodo: '', 
+            idForTodo: 3,
             tasks: [
                 {
                     id: 1,
-                    title: 'Example task',
+                    title: 'Acividad 1 ',
+                    done: false
+                },
+                {
+                    id: 2,
+                    title: 'Example2 task2',
                     done: false
                 }
             ]
@@ -35,7 +63,19 @@ export default {
     },
 
     methods: {
+       addTodo(){
+           if(this.newTodo.trim().length == 0){
+               return
+           }
 
+           this.tasks.push({
+               id: this.idForTodo,
+               title: this.newTodo,
+               done: false,
+           })
+        this.newTodo=''
+        this.idForTodo++
+    }
     }
 }
 </script>
